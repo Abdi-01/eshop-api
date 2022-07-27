233301f93fa7c9b767d2f476aa5b85e277b12403
Abdi-01 <abdialghi@gmail.com>
2022-07-27 14:48:19 +0700

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,7 +33,31 @@ module.exports = {
         })
     },
     login: (req, res) => {
+        let { email, password } = req.body;
+        dbConf.query(`Select u.iduser, u.username, u.email, u.age, u.city, u.status_id, s.status from users u JOIN status s on u.status_id = s.idstatus 
+        WHERE u.email=${dbConf.escape(email)} 
+        and u.password=${dbConf.escape(hashPassword(password))};`, (err, results) => {
+            if (err) {
+                console.log('Error query SQL :', err);
+                res.status(500).send(err);
+            }
 
+            dbConf.query(`Select u.iduser, p.idproduct, p.name, p.images, p.brand, 
+            p.category, p.price, c.qty, p.price*c.qty as totalPrice from users u
+            JOIN carts c ON u.iduser=c.user_id
+            JOIN products p ON p.idproduct = c.product_id WHERE c.user_id =${dbConf.escape(results[0].iduser)};`,(errCart,resultsCart)=>{
+                if (errCart) {
+                    console.log('Error query SQL :', err);
+                    res.status(500).send(err);
+                }
+
+                res.status(200).send({
+                    ...results[0],
+                    cart:resultsCart
+                })
+
+            })
+        })
     },
     keepLogin: (req, res) => {
 
