6713b8e619114457b3254a22e2ef21630e8ac7ba
Abdi-01 <abdialghi@gmail.com>
2022-07-28 14:31:48 +0700

    product

--- controllers/product.js
+++ controllers/product.js
@@ -1,15 +1,16 @@
-const { dbConf } = require("../config/db")
+const { dbConf, dbQuery } = require("../config/db")
 
 
 module.exports = {
-    getData: (req, res) => {
-        dbConf.query(`Select p.*, s.status from products p 
-        JOIN status s on p.status_id=s.idstatus;`,(err, results)=>{
-            if(err){
-                console.log(err);
-                res.status(500).send(err);
-            }
-            res.status(200).send(results)
-        })
+    getData: async (req, res) => {
+        try {
+            let resultsGet = await dbQuery(`Select p.*, s.status from products p 
+            JOIN status s on p.status_id=s.idstatus;`)
+            
+            res.status(200).send(resultsGet)
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
     }
 }
