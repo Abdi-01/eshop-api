597ff37736262d6996c508a92e84a3853f01a0fb
Abdi-01 <abdialghi@gmail.com>
2022-07-28 15:20:13 +0700

    product

--- controllers/product.js
+++ controllers/product.js
@@ -6,11 +6,40 @@ module.exports = {
         try {
             let resultsGet = await dbQuery(`Select p.*, s.status from products p 
             JOIN status s on p.status_id=s.idstatus;`)
-            
+
             res.status(200).send(resultsGet)
         } catch (error) {
             console.log(error);
             res.status(500).send(error);
         }
+    },
+    deleteData: async (req, res) => {
+        try {
+            await dbQuery(`DELETE from products where idproduct=${req.params.id}`);
+
+            res.status(200).send({
+                success: true,
+                message: 'Delete product'
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    },
+    add: async (req, res) => {
+        let dataInput = [];
+        for(const prop in req.body){
+            dataInput.push(dbConf.escape(req.body[prop]))
+        }
+        let addData = await dbQuery(`INSERT INTO products (name, brand, category, description, images, stock, price) 
+        values (${dataInput.join(',')});`);
+
+        res.status(200).send({
+            success:true,
+            message:'Add product Success'
+        })
+    },
+    update: async (req, res) => {
+
     }
 }
--- routers/product.js
+++ routers/product.js
@@ -3,5 +3,8 @@ const { productController } = require('../controllers');
 const route = express.Router();
 
 route.get('/', productController.getData)
+route.post('/', productController.add)
+route.delete('/:id', productController.deleteData)
+route.patch('/:id', productController.update)
 
 module.exports = route
