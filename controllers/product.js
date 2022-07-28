a851f9e6b1d4805ab743762f0df1cd5b393307c6
Abdi-01 <abdialghi@gmail.com>
2022-07-28 14:13:32 +0700

    product

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,15 @@
+const { dbConf } = require("../config/db")
 
 
 module.exports = {
     getData: (req, res) => {
-
+        dbConf.query(`Select p.*, s.status from products p 
+        JOIN status s on p.status_id=s.idstatus;`,(err, results)=>{
+            if(err){
+                console.log(err);
+                res.status(500).send(err);
+            }
+            res.status(200).send(results)
+        })
     }
 }
